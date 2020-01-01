19df2a3a6f639bbe1ff8338037313a7a487252b7
ewu001 <59420306+ewu001@users.noreply.github.com>
2020-01-01 15:01:26 -0500

    bankruptcy project initial checkin

---
+++ Financial bankruptcy prediction/model.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.naive_bayes import GaussianNB
+
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc, roc_auc_score
+from sklearn.model_selection import train_test_split, KFold, GridSearchCV, cross_val_score
+
+
+
+def logistic_regression():
+    lr = LogisticRegression()
+    return lr
+
+def naive_bayes():
+    naive_bayes_clf = GaussianNB()
+    return naive_bayes_clf
+
+def random_forest():
+    random_forest_clf = RandomForestClassifier()
+    return random_forest_clf
+
+def decision_tree():
+    decision_tree_clf = DecisionTreeClassifier()
+    return decision_tree_clf
+
+def gradient_boosting():
+    gradient_boost_clf = GradientBoostingClassifier()
+    return gradient_boost_clf
+
+def train_tuning(model, data_X, data_y, hparam={}, cvmetric=""):
+    X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, random_state=42, test_size=0.2)
+    print("X_train size: ", X_train.shape)
+    if hparam != {}:
+        model = GridSearchCV(model, hparam, cv=5, return_train_score=True, scoring=cvmetric)
+        print("best parameter: ", model.best_params_)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print('Accuracy : ', accuracy_score(y_test, y_pred))
+    print(confusion_matrix(y_test, y_pred))
+    return model
+
+def train_kfold_cv(model, data_X, data_y, kfold, metric):
+    num_folds = kfold
+    score = metric
+    kfold = KFold(n_splits=num_folds, shuffle=False, random_state=42)
+    CV_score = cross_val_score(model, data_X, data_y, cv=kfold, scoring=score)
+
+    print("CV with K-Fold " + metric + " score: %0.3f (+/- %0.2f)" % (CV_score.mean(), CV_score.std() * 2))
+    return model
+
+def roc_auc_plot(model, data_X, data_y):
+    X_train, X_test, y_train, y_test = train_test_split(data_X, data_y, random_state=42, test_size=0.2)
+    probs = model.predict_proba(X_test)
+    preds = probs[:,1]
+    fpr, tpr, threshold = roc_curve(y_test, preds)
+    roc_auc = auc(fpr, tpr)
+
+    # plot the curve
+    plt.figure()
+    lw = 2
+
+    plt.plot(fpr, tpr, color='red', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='black', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic')
+    plt.legend(loc="lower right")
+
+def skmodel_evaluate(model, eval_X, eval_y):
+    y_pred = model.predict(eval_X)
+    print('Accuracy : ', accuracy_score(eval_y, y_pred))
+    print(confusion_matrix(eval_y, y_pred))
---
+++ Financial bankruptcy prediction/util.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.utils import shuffle
+
+def df_explore_helper(df):
+    print("Shape of dataframe: {} ".format(df.shape))
+    df_summary = pd.DataFrame(df.dtypes, columns=['dtypes'])
+    df_summary = df_summary.reset_index()
+    df_summary['Name'] = df_summary['index']
+    df_summary = df_summary[['Name', 'dtypes']]
+
+    # explore this dataframe
+    df_summary['Missing values'] = df.isnull().sum().values
+    df_summary['Unique values'] = df.nunique().values
+    df_summary['First Value'] = df.iloc[0].values
+  
+    for name in df_summary['Name'].value_counts().index:
+        isNumeric = (df[name].dtype == np.float64 or df[name].dtype == np.int64)
+    
+        if isNumeric:
+            df_summary.loc[df_summary['Name']==name, 'Minimum'] = df[name].min()
+            df_summary.loc[df_summary['Name']==name, 'Maximum'] = df[name].max()
+    return df_summary
+
+
+def eval_train_generator(dataframe, target, split_number):
+    
+    true_record = dataframe[dataframe[target]==1]
+    false_record = dataframe[dataframe[target]==0]
+
+    false_record = shuffle(false_record)
+    true_record = shuffle(true_record)
+
+    false_eval = false_record.iloc[0:split_number]
+    true_eval = true_record.iloc[0:split_number]
+
+    false_train = false_record.iloc[split_number+1:]
+    true_train = true_record.iloc[split_number+1:]
+
+    eval_dataset = pd.concat([true_eval, false_eval])
+    eval_dataset = shuffle(eval_dataset)
+
+    train_dataset =  pd.concat([true_train, false_train])
+    train_dataset = shuffle(train_dataset)
+
+    print(eval_dataset.shape)
+    print(train_dataset.shape)
+
+    eval_dataset.to_csv("data store/evaluation_dataset.csv", encoding='utf-8')
+    train_dataset.to_csv("data store/training_dataset.csv", encoding='utf-8')
+    print("Completed")
+    return True
